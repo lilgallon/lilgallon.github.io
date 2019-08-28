ec0976ab03470b995c571004a47ae261fdb2fad6
nero <swellchaser33@gmail.com>
2019-08-28 19:48:10 +0200

    added light theme & theme switcher

--- resources/js/theme-switcher.js
+++ resources/js/theme-switcher.js
@@ -4,13 +4,19 @@
 
 // executes when HTML-Document is loaded and DOM is ready
 document.addEventListener("DOMContentLoaded", function (event) {
+
+    document.getElementById('original-theme').disabled = false;
+    document.getElementById('light-theme').disabled = true;
+
     // By default, the switcher is off and the theme is "original"
     let theme_switcher = document.getElementById("theme-switcher");
     theme_switcher.addEventListener('change', function(){
         if(this.checked) {
-            // light theme
+            document.getElementById('original-theme').disabled = true;
+            document.getElementById('light-theme').disabled = false;
         } else {
-            // original theme
+            document.getElementById('original-theme').disabled = false;
+            document.getElementById('light-theme').disabled = true;
         }
     })
 });
