04a0c1653d18ac62c03936779be090f0d0790f13
Lilian Gallon <swellchaser33@gmail.com>
2019-11-25 00:51:51 -0800

    added twitter & stackoverflow activity

--- js/index.js
+++ js/index.js
@@ -10,31 +10,54 @@ let vue = new Vue({
             {name: "Post1", icon: "fas fa-newspaper"},
             {name: "Post2", icon: "fas fa-newspaper"}
         ],
-        activities: [],
-        activitiesC: 0
+        activitiesGH: [],
+        activitiesGHcount: 0,
+        activitiesSO: []
     },
     methods: {
-        updateRecentActivity(increment) {
-            this.activitiesC += increment
+        updateRecentGithubActivity(increment) {
+            this.activitiesGHcount += increment
             axios.get("https://api.github.com/users/N3ROO/events/public")
             .then((response) => {
-                this.activities = [];
-                for(let i = 0; i < this.activitiesC; i ++) {
+                this.activitiesGH = [];
+                for(let i = 0; i < this.activitiesGHcount; i ++) {
                     let d = response.data[i];
 
+                    if (d === undefined) break;
+
                     let activity = {
-                        repo_url: d.repo.url,
+                        repo_url: d.repo.url.replace("api.github.com/repos/", "github.com/"),
                         repo_name: d.repo.name,
                         type: d.type.replace("Event", ""),
                         date: new Date(d.created_at).toLocaleString()
                     }
 
-                    this.activities.push(activity);
+                    this.activitiesGH.push(activity);
                 }
             });
+        },
+        updateRecentStackOverflowActivity(){
+            axios.get("https://api.stackexchange.com/2.2/users/8811838/answers?order=desc&sort=activity&site=stackoverflow")
+            .then((response) => {
+                this.activitiesSO = [];
+                for(let i = 0; i < 12; i++) {
+                    let d = response.data.items[i];
+
+                    if (d === undefined) break;
+
+                    let activity = {
+                        score: d.score,
+                        date: new Date(d.creation_date*1000).toLocaleDateString("en-US"),
+                        url: "https://stackoverflow.com/questions/" + d.question_id
+                    }
+
+                    this.activitiesSO.push(activity)
+                }
+            })
         }
     },
     mounted() {
-        this.updateRecentActivity(5)
+        this.updateRecentGithubActivity(5);
+        this.updateRecentStackOverflowActivity();
     }
 });
