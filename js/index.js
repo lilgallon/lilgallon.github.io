040c70260f5af66567a214acae3429802e198c87
Lilian Gallon <swellchaser33@gmail.com>
2019-11-25 00:03:24 -0800

    added recent activities & did a lot of other things

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-let projects = [
-
-]
 
 let vue = new Vue({
     el: '#app',
@@ -12,6 +9,32 @@ let vue = new Vue({
         posts: [
             {name: "Post1", icon: "fas fa-newspaper"},
             {name: "Post2", icon: "fas fa-newspaper"}
-        ]
+        ],
+        activities: [],
+        activitiesC: 0
+    },
+    methods: {
+        updateRecentActivity(increment) {
+            this.activitiesC += increment
+            axios.get("https://api.github.com/users/N3ROO/events/public")
+            .then((response) => {
+                this.activities = [];
+                for(let i = 0; i < this.activitiesC; i ++) {
+                    let d = response.data[i];
+
+                    let activity = {
+                        repo_url: d.repo.url,
+                        repo_name: d.repo.name,
+                        type: d.type.replace("Event", ""),
+                        date: new Date(d.created_at).toLocaleString()
+                    }
+
+                    this.activities.push(activity);
+                }
+            });
+        }
+    },
+    mounted() {
+        this.updateRecentActivity(5)
     }
 });
