de0fb12db98766b0496d5b66677a65457e642906
Lilian Gallon <swellchaser33@gmail.com>
2019-12-02 23:56:22 -0800

    added blog page

---
+++ js/blog.js
@@ -0,0 +1,44 @@
+
+let vue = new Vue({
+    el: '#app',
+    data: {
+        /* GENERAL */
+        version: "2019.1.0b",
+
+        /* SIDEBAR */
+        // ...
+
+
+        /* MIDDLE CONTENT */
+        // ...
+
+        /* RIGHT CONTENT */
+        // ...
+    },
+    methods: {
+        /* GENERAL */
+        // ...
+
+        /* SIDEBAR */
+        //...
+
+        /* MIDDLE CONTENT */
+        // ...
+
+        /* RIGHT CONTENT */
+        // ...
+    },
+    mounted() {
+        /* GENERAL */
+        // ...
+
+        /* SIDEBAR */
+        //...
+
+        /* MIDDLE CONTENT */
+        // ...
+
+        /* RIGHT CONTENT */
+        // ...
+    }
+});
