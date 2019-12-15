9f9f9108078bc07cc131db61a9410dd535b61104
nero <swellchaser33@gmail.com>
2019-12-15 10:17:31 +0100

    added playlists page

---
+++ js/playlists.js
@@ -0,0 +1,44 @@
+
+let vue = new Vue({
+    el: '#app',
+    data: {
+        /* GENERAL */
+        version: "2019.1.1",
+
+        /* SIDEBAR */
+        // ...
+
+
+        /* MIDDLE CONTENT */
+        // ...
+
+        /* RIGHT CONTENT */
+        // ...
+    },
+    methods: {
+        /* GENERAL */
+        // ...
+
+        /* SIDEBAR */
+        //...
+
+        /* MIDDLE CONTENT */
+        // ...
+
+        /* RIGHT CONTENT */
+        // ...
+    },
+    mounted() {
+        /* GENERAL */
+        // ...
+
+        /* SIDEBAR */
+        //...
+
+        /* MIDDLE CONTENT */
+        // ...
+
+        /* RIGHT CONTENT */
+        // ...
+    }
+});
