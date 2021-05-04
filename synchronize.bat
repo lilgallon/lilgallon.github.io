f7f4f9b583ca29161c74f14472e21d12ae77d81d
Gallon Lilian <lilian@gallon.dev>
2021-05-04 18:18:02 +0200

    synchronization

--- synchronize.bat
+++ synchronize.bat
@@ -3,7 +3,16 @@ call python .\synchronize.py
 cd ..\apindex\
 call .\update.bat
 cd ..\..
+
+echo Push to git? (y/n)
+set /P INPUT=Type input: %=%
+If /I "%Input%"=="y" goto yes
+goto no
+
+:yes
 call git add .
 call git commit -m "synchronization"
 call git push origin master
+
+:no
 pause
