87056b2cc74a0f261182e4b689d1c027786a49c4
Gallon Lilian <lilian@gallon.dev>
2021-05-10 16:56:12 +0200

    synchronization

--- synchronize.bat
+++ synchronize.bat
@@ -1,15 +1,14 @@
-cd .\python\synch_repos\
-call python .\synchronize.py
-cd ..\apindex\
-call .\update.bat
-cd ..\..
-
-echo Push to git? (y/n)
+echo Synchronize website? (y/n)
 set /P INPUT=Type input: %=%
 If /I "%Input%"=="y" goto yes
 goto no
 
 :yes
+cd .\python\synch_repos\
+call python .\synchronize.py
+cd ..\apindex\
+call .\update.bat
+cd ..\..
 call git add .
 call git commit -m "synchronization"
 call git push origin master
